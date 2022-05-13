20da30f79d9f95b097513ccf41461358be71e8dc
Sregei <sergeipodlas@gmail.com>
2022-05-13 18:33:58 +0300

    Code refactoring (the test works)

--- test_add_group.py
+++ test_add_group.py
@@ -9,17 +9,24 @@ class TestAddGroup(unittest.TestCase):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
 
-    def test_add_group(self):
-        wd = self.wd
+    def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/")
+
+    def login(self, wd):
         wd.find_element(By.NAME, "user").click()
         wd.find_element(By.NAME, "user").clear()
         wd.find_element(By.NAME, "user").send_keys("admin")
         wd.find_element(By.NAME, "pass").clear()
         wd.find_element(By.NAME, "pass").send_keys("secret")
         wd.find_element(By.XPATH, "//input[@value='Login']").click()
+
+    def open_groups_page(self, wd):
         wd.find_element(By.LINK_TEXT, "groups").click()
+
+    def create_group(self, wd):
+        # init group creation
         wd.find_element(By.NAME, "new").click()
+        # fill group form
         wd.find_element(By.NAME, "group_name").click()
         wd.find_element(By.NAME, "group_name").clear()
         wd.find_element(By.NAME, "group_name").send_keys("dfgdfg")
@@ -29,9 +36,23 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element(By.NAME, "group_footer").click()
         wd.find_element(By.NAME, "group_footer").clear()
         wd.find_element(By.NAME, "group_footer").send_keys("dfgdfgdfg")
+        # submit group creation
         wd.find_element(By.NAME, "submit").click()
+
+    def return_to_groups_page(self, wd):
         wd.find_element(By.LINK_TEXT, "groups").click()
+
+    def logout(self, wd):
         wd.find_element(By.LINK_TEXT, "Logout").click()
+
+    def test_add_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_groups_page(wd)
+        self.create_group(wd)
+        self.return_to_groups_page(wd)
+        self.logout(wd)
 
     def tearDown(self):
         self.wd.quit()
