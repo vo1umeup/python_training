539c1c958453741fe2948d7291ba7c7192dc8c93
Sregei <sergeipodlas@gmail.com>
2022-05-17 12:39:47 +0300

    Созданиы пакеты для разных зон ответственности

---
+++ test/test_add_group.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import pytest
+from model.group import Group
+from fixture.application import Application
+
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="dfgdfg", header="dfgdfg", footer="dfgdfgdfg"))
+    app.logout()
+
+
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
