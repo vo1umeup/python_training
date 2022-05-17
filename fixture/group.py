93a654b8d767e7affa2842faf99e8450211d30fc
Sregei <sergeipodlas@gmail.com>
2022-05-17 19:13:54 +0300

    До параметра (scope = "session") в файле "conftest" сам тест срабатывает, а с параметром нет

--- fixture/group.py
+++ fixture/group.py
@@ -29,6 +29,17 @@ class GroupHelper:
         wd.find_element(By.NAME, "submit").click()
         self.return_to_groups_page()
 
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element(By.NAME, "selected[]").click()
+        # submit deletion
+        wd.find_element(By.NAME, "delete").click()
+        self.return_to_groups_page()
+
+
     def return_to_groups_page(self):
         wd = self.app.wd
         wd.find_element(By.LINK_TEXT, "groups").click()
